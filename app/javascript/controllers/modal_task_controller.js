eba5312daa549ffa5cd19b7fa850c1b5b5999182
Jean Pier <diegojeanpier.dc.@gmail.com>
2023-01-14 10:39:24 -0500

    add stimulus controller

---
+++ app/javascript/controllers/modal_task_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="modal-task"
+export default class extends Controller {
+  showModal(){
+    const modal = new bootstrap.Modal("#taskModal");
+    modal.show();
+    
+    document.getElementById('taskModal').addEventListener('hidden.bs.modal', event => {
+      document.getElementById('show_content_modal').innerHTML = `
+        <div class="modal-header">
+          <h5 class="card-title placeholder-glow">
+            <span class="placeholder col-6"></span>
+          </h5>
+        </div>
+        <div class="modal-body">
+          <p class="card-text placeholder-glow">
+            <span class="placeholder col-7"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-6"></span>
+            <span class="placeholder col-8"></span>
+          </p>
+          <p class="card-text placeholder-glow">
+            <span class="placeholder col-7"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-6"></span>
+            <span class="placeholder col-8"></span>
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary disabled placeholder col-1"></button>
+        </div>
+      `
+    })
+  }
+}
